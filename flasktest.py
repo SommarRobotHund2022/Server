25e21fd39092eafb43935a182e0aeefbb2e8e930
Martin Olivesten <m.olivesten@protonmail.com>
2022-06-16 14:11:13 +0200

    Started on livefeed feature

--- flasktest.py
+++ flasktest.py
@@ -2,8 +2,23 @@ from flask import Flask, Response, request
 import ardSerial as ard
 import subprocess
 import os
+import cv2
+from picamera import PiCamera
 
+
+camera = PiCamera()
 app = Flask(__name__)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route('/')
 def index():
@@ -21,4 +36,9 @@ def forward():
     except:
         return "👎"
 
-app.run()
+@app.route('/feed')
+def feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0")
